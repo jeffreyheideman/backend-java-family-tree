6e49ae6d25faff16cab7cbab5299584da20c8104
Jeffrey Heideman <jeffrey_heideman@hotmail.com>
2023-10-23 13:29:12 +0200

    Alle tests af Denk ik

--- src/test/java/nl/novi/PersonTest.java
+++ src/test/java/nl/novi/PersonTest.java
@@ -3,6 +3,9 @@ package nl.novi;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PersonTest {
@@ -10,12 +13,21 @@ class PersonTest {
     Person father;
     Person mother;
     Person me;
+    Person sister;
+    Pet Dog;
+    Person nephew;
+    Person grandMother;
 
     @BeforeEach
     void setUp() {
         father = new Person("Frans", "Janwillem", "Heideman", 63, 'm');
         me = new Person("Jeffrey", "Heideman", 27, 'm');
+        sister = new Person("Sylvia", "Heideman", 29, 'f');
         mother = new Person("Marijke", "Versteeg", 62, 'f');
+        Dog = new Pet("Dog", 2, "Malteser");
+        nephew = new Person("Sven", "Van der Poel", 4, 'm');
+        grandMother = new Person("Jakoba", "Heideman", 90, 'f');
+
 
 
 
@@ -23,10 +35,21 @@ class PersonTest {
 
     @Test
     void getChildren() {
+        List<Person> children = new ArrayList<>();
+        children.add(me);
+        children.add(sister);
+        father.setChildren(children);
+        assertEquals(children, father.getChildren());
+
     }
 
     @Test
     void setChildren() {
+        List<Person> children = new ArrayList<>();
+        children.add(me);
+        children.add(sister);
+        father.setChildren(children);
+        assertEquals(children, father.getChildren());
     }
 
     @Test
@@ -73,50 +96,80 @@ class PersonTest {
 
     @Test
     void getAge() {
+        int getAge = father.getAge();
+        assertEquals(63, getAge);
     }
 
     @Test
     void setAge() {
+        father.setAge(64);
+        int getAge = father.getAge();
+        assertEquals(64, getAge);
     }
 
     @Test
     void getSex() {
+        char getSex = father.getSex();
+        assertEquals('m', getSex);
     }
 
     @Test
     void setSex() {
+        father.setSex('f');
+        char getSex = father.getSex();
+        assertEquals('f', getSex);
     }
 
     @Test
     void getMother() {
+        mother.setMother(mother);
+        assertEquals(mother, mother);
     }
 
     @Test
     void setMother() {
+        mother.setMother(mother);
+        Person getMother = mother.getMother();
+        assertEquals(mother, getMother);
     }
 
     @Test
     void getFather() {
+        father.setFather(father);
+        assertEquals(father, father);
     }
 
     @Test
     void setFather() {
+        father.setFather(father);
+        Person getFather = father.getFather();
+        assertEquals(father, getFather);
     }
 
     @Test
     void getSiblings() {
+        List<Person> kids = new ArrayList<>();
+        kids.add(me);
+        kids.add(sister);
+        father.setChildren(kids);
+
     }
 
     @Test
     void setSiblings() {
+        List<Person> kids = new ArrayList<>();
+        kids.add(me);
+        kids.add(sister);
+        father.setChildren(kids);
+        List<Person> getSiblings = father.getChildren();
+        assertEquals(kids, getSiblings);
     }
 
     @Test
     void getPets() {
-    }
-
-    @Test
-    void setPets() {
+        List<Pet> pets = new ArrayList<>();
+        pets.add(Dog);
+        me.setPets(pets);
     }
 
     @Test
@@ -129,17 +182,34 @@ class PersonTest {
 
     @Test
     void addChildToChildren() {
+        List<Person> children = new ArrayList<>();
+        children.add(sister);
+        sister.addChildToChildren(sister, nephew);
     }
 
     @Test
     void addPet() {
+        me.addPet(me, Dog);
+        assertEquals(Dog, me.getPets().get(0));
     }
 
     @Test
     void addSibling() {
+        List<Person> siblings = new ArrayList<>();
+        siblings.add(sister);
+        father.setChildren(siblings);
+        assertEquals(siblings, father.getChildren());
     }
 
     @Test
     void getGrandChildren() {
+        List<Person> grandChildren = new ArrayList<>();
+        grandChildren.add(nephew);
+
+        sister.addChildToChildren(sister, nephew);
+        father.addChildToChildren(father, sister);
+        List<Person> list = father.getGrandChildren(father);
+        assertEquals(grandChildren, list);
+
     }
 }
